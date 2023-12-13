8369f67cd5910793879c9c56c5727e74f87d99eb
salsabeelnoor <salsabeelnoor@gmail.com>
2023-12-14 01:01:03 +0600

    first

---
+++ src/components/Player.jsx
@@ -0,0 +1,22 @@
+import { useState, useRef } from "react";
+export default function Player() {
+  const playerName = useRef();
+  console.log(playerName)
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  function handleClick(){
+    // setSubmitted(true)
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+      <p>
+        <input 
+        ref={playerName} 
+        type="text" />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,14 @@
+import { forwardRef } from "react";
+const ResultModal = forwardRef( function ResultModal({ result, targetTime}, ref){
+    return(
+        <dialog ref={ref} className="result-modal">
+            <h2>You {result}</h2>
+            <p>The target time was <strong>{targetTime}</strong> seconds.</p>
+            <p>You stopped the timer with <strong>X seconds left</strong> </p>
+            <form action="" method="dialog">
+                <button>Close</button>
+            </form>
+        </dialog>
+    );
+})
+export default ResultModal;
